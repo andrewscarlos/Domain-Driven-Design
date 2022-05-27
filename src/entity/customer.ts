700a29ea712f54ef29ac8389bf1c4807de1517e9
andrewscarlos <andrews.knct@gmail.com>
2022-05-27 00:58:19 -0300

    feat: add customer entity

---
+++ src/entity/customer.ts
@@ -0,0 +1,36 @@
+export class Customer {
+    _id : string;
+    _name : string;
+    _address : string = '';
+    _active : boolean = true;
+
+    constructor (id : string , name : string) {
+        this._id = id;
+        this._name = name;
+        this.validate ()
+    }
+
+    validate () {
+        if ( this._name.length === 0 ) {
+            throw new Error ( 'Name is required' );
+        }
+        if ( this._id.length === 0 ) {
+            throw new Error ( 'Id is required' );
+        }
+    }
+
+    changeName (name : string) {
+        this._name = name;
+        this.validate ();
+    }
+
+    activate () {
+        if ( this._address.length === 0 ) {
+            throw new Error ( 'Address is required' );
+        }
+        this._active = true;
+    }
+
+}
+
+let customer = new Customer ( '1' , 'John' );
