54263c03ee947c17d976d34f3ef0d2967f908333
andrewscarlos <andrews.knct@gmail.com>
2022-05-29 23:23:02 -0300

    test: add prduct class and product tests

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,21 @@
+import { Product } from "./product";
+
+describe("Product unit tests", () => {
+    it("should throw error when id is empty", () => {
+        expect(()=>{
+            new Product("", "Product 1", 100);
+        }).toThrowError("Id is required");
+    })
+
+    it("should throw error when name is empty", () => {
+        expect(()=>{
+            new Product("123", "", 100);
+        }).toThrowError("Name is required");
+    })
+
+    it("should throw error when price is less than zero", () => {
+        expect(()=>{
+            new Product("123", "Product 1", 0);
+        }).toThrowError("Price must be greater than 0");
+    })
+})
